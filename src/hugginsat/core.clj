edd707a2977ac232217ee819ad8e48af32309bb9
Andrew Huggins <ahuggins@torchtechnologies.com>
2022-05-04 16:09:16 -0500

    removed prototyping

--- src/hugginsat/core.clj
+++ src/hugginsat/core.clj
@@ -7,19 +7,12 @@
    [manifold.stream :as s]
    [clojure.edn :as edn]
    [clojure.java.shell :as shell]
-   [tilakone.core :as tk :refer [_]]
    [clojure.string :as str]))
 
-;; ping something
+;;ping to make sure there is something to connect to
 (defn ping-something [something] (shell/sh "ping" "-c" "1" "-W" "3" something))
 
-
-
 (def addresses (:addresses (edn/read-string (slurp "resources/config.edn"))))
-(def p (map (comp first ping-something) addresses))
-
-addresses
-p
 
 (defn check-addresses [addresses]
   (let [p (mapv (comp #(if (= (second %) 0) true false) first ping-something) addresses)
@@ -27,23 +20,14 @@ p
     (zipmap k p)
   ))
 
-(check-addresses addresses)
-
 
 (def ping-state {:ping-state (check-addresses addresses)})
 
-ping-state
 
-(-> ping-state :ping-state :10.10.3.200)
+;; using aleph to create clients for loop-back testing
 
-
-;; using aleph
-
-(def vector-bytes-frame
-  "Frames are just clojure data structures, this frame is a vector of bytes."
-  [:byte :byte :byte :byte :byte :byte :byte :byte :byte ])
-;;(def protocol (gloss/compile-frame vector-bytes-frame))
 (def protocol (gloss/compile-frame (gloss/string :ascii)))
+
 (defn wrap-duplex-stream
   [protocol s]
   (let [out (s/stream)]
@@ -54,52 +38,13 @@ ping-state
      out
      (io/decode-stream s protocol))))
 
-
 (defn client
   [host port]
   (d/chain (tcp/client {:host host, :port port})
     #(wrap-duplex-stream protocol %)))
 
 
-(defn start-server
-  [handler port]
-  (tcp/start-server
-    (fn [s info]
-      (handler (wrap-duplex-stream protocol s) info))
-    {:port port}))
-
-(defn fast-echo-handler
-  [f]
-  (fn [s info]
-    (s/connect
-      (s/map f s)
-      s)))
-
-;;servers
-(def s
-  (start-server
-    (fast-echo-handler str)
-    4762))
-(def s2
-  (start-server
-   (fast-echo-handler str)
-   10002))
-
-;;clients
-(def c @(client "10.10.1.40" 4760))
-
-(def c2 @(client "10.10.1.40" 4762))
-
 (defn try-client [ip port] (try @(client ip port) (catch Exception e (str "caught exception: " (.getMessage e)))))
-
-(def c-try (try-client "localhost" 4762))
-(s/put! c-try "hello")
-(type c-try)
-(def loop-conf {:loop-back-config {:ip "10.10.1.40" :send-port 4760 :receive-port 4762 :data "hello clojure!"}})
-
-(def loop-local-conf {:loop-back-config {:ip "localhost" :send-port 4760 :receive-port 4762 :data "hello clojure!\n"}})
-(def loop-local-same {:loop-back-config {:ip "localhost" :send-port 4762 :receive-port 4762 :data "hello clojure!\n"}})
-
 
 (defn loop-back-test [config]
   (let [{:keys [ip send-port receive-port data]} config
@@ -113,39 +58,9 @@ ping-state
   )
 )
 
-(loop-back-test (:loop-back-config loop-local-conf))
-(loop-back-test (:loop-back-config loop-local-same))
-
-;; test all configs and merge results
-(def loopbacks (:loop-back-configs (edn/read-string (slurp "resources/config.edn"))))
-loopbacks
-(map #({(key %) (loop-back-test (val %))}) loopbacks)
-(zipmap (keys loopbacks) (map loop-back-test (vals loopbacks)))
-
-(def lbs {:loop-back-state  (loop-back-test "10.10.1.40" 4760 4762 "1")})
-lbs
-
-(merge ping-state {:loop-back-state(zipmap (keys loopbacks) (map loop-back-test (vals loopbacks)))})
+;;test all loop-back configs and merge results
+(def loop-backs (:loop-back-configs (edn/read-string (slurp "resources/config.edn"))))
 
 
-(def c3 @(client "localhost" 4762))
-(def c4 @(client "localhost" 4762))
+(def connection-report {:report (merge ping-state {:loop-back-state(zipmap (keys loop-backs) (map loop-back-test (vals loop-backs)))})})
 
-(s/put! c "1")
-;;(s/put! c  (map unchecked-byte [0xfe 0x6b 0x28 0x40 0x82 0x00 0x00 0x00 0x01]))
-;;(s/put! c "hello world")
-;;(s/put! c3 (map unchecked-byte [0xfe 0x6b 0x28 0x40 0x82 0x00 0x00 0x00 0x01]))
-(s/put! c3 "how are you?")
-;;(s/put! c2 "1a")
-
-;;(s/take! c)
-@(s/take! c3)
-;;deref stream to get value
-(def answer @(s/take! c3))
-
-(= "how are you?\n" answer )
-(s/put! c4 "hello from c4")
-(s/take! c4)
-
-(.close s)
-(.close s2)
